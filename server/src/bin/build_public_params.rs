b79bd935f955dc35cb1da2a7a1fc483ebc34d8b7
AntoineF4C5 <antoinedcht@hotmail.com>
2024-08-27 19:16:58 +0200

    Try to send proof to verifier and have him verify it

---
+++ server/src/bin/build_public_params.rs
@@ -0,0 +1,63 @@
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+
+use nova::provider::PallasEngine;
+use std::path::PathBuf;
+
+use zk_engine::{
+    args::{WASMArgsBuilder, WASMCtx},
+    nova::{
+        provider::ipa_pc,
+        spartan::{self, snark::RelaxedR1CSSNARK},
+        traits::Dual,
+    },
+    run::batched::BatchedZKEProof,
+    traits::zkvm::ZKVM,
+    utils::logging::init_logger,
+};
+
+// Curve cycle to use for proving
+type E1 = PallasEngine;
+// PCS used for final SNARK at the end of (N)IVC
+type EE1<E> = ipa_pc::EvaluationEngine<E>;
+// PCS on secondary curve
+type EE2<E> = ipa_pc::EvaluationEngine<Dual<E>>;
+
+// Spartan SNARKS used for compressing at then end of (N)IVC
+type BS1<E> = spartan::batched::BatchedRelaxedR1CSSNARK<E, EE1<E>>;
+type S1<E> = RelaxedR1CSSNARK<E, EE1<E>>;
+type S2<E> = RelaxedR1CSSNARK<Dual<E>, EE2<E>>;
+
+fn main() {
+    // Configure the arguments needed for WASM execution
+    //
+    // Here we are configuring the path to the WASM file
+    let args = WASMArgsBuilder::default()
+        .file_path(PathBuf::from("wasm/fib.wat"))
+        .invoke(Some(String::from("fib")))
+        .func_args(vec![String::from("10")]) // This will generate 16,000 + opcodes
+        .build();
+
+    // Create a WASM execution context for proving.
+    let mut wasm_ctx = WASMCtx::new_from_file(args).unwrap();
+
+    // Prove execution and run memory consistency checks
+    //
+    // Get proof for verification and corresponding public values
+    //
+    // Above type alias's (for the backend config) get used here
+    let (proof, public_values, _) =
+        BatchedZKEProof::<E1, BS1<E1>, S1<E1>, S2<E1>>::prove_wasm(&mut wasm_ctx).unwrap();
+
+    let proof_string = serde_json::to_string(&proof).unwrap();
+    std::fs::create_dir("public_params");
+    save_to_file("public_params/public_params.json", &proof_string);
+}
+
+fn save_to_file(filename: &str, data: &str) -> anyhow::Result<()> {
+    let mut file = File::create(filename)?;
+    file.write_all(data.as_bytes())?;
+    println!("Data written to {}", filename);
+    Ok(())
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,41 @@
+use std::fs::read_to_string;
+
 use axum::{
-    routing::{get, post},
     http::StatusCode,
+    routing::{get, post},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
 
+use nova::provider::PallasEngine;
+
+use zk_engine::{
+    args::{WASMArgsBuilder, WASMCtx},
+    nova::{
+        provider::ipa_pc,
+        spartan::{self, snark::RelaxedR1CSSNARK},
+        traits::Dual,
+    },
+    run::batched::{public_values::BatchedPublicValues, BatchedZKEProof},
+    traits::zkvm::ZKVM,
+    // utils::logging::init_logger,
+};
+
+// Curve cycle to use for proving
+type E1 = PallasEngine;
+// PCS used for final SNARK at the end of (N)IVC
+type EE1<E> = ipa_pc::EvaluationEngine<E>;
+// PCS on secondary curve
+type EE2<E> = ipa_pc::EvaluationEngine<Dual<E>>;
+
+// Spartan SNARKS used for compressing at then end of (N)IVC
+type BS1<E> = spartan::batched::BatchedRelaxedR1CSSNARK<E, EE1<E>>;
+type S1<E> = RelaxedR1CSSNARK<E, EE1<E>>;
+type S2<E> = RelaxedR1CSSNARK<Dual<E>, EE2<E>>;
+
 #[tokio::main]
 async fn main() {
-    // initialize tracing
+    // // initialize tracing
     // tracing_subscriber::fmt::init();
 
     // build our application with a route
@@ -15,10 +43,12 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/post", post(test_post));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -27,31 +57,34 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
+async fn test_post(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
+    Json(proof): Json<BatchedZKEProof<E1, BS1<E1>, S1<E1>, S2<E1>>>,
+) -> (StatusCode, Json<VerifyResult>) {
     // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
+    let public_params_str = read_to_string("public_params/public_params.json").unwrap();
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+    let public_params = serde_json::from_str::<BatchedPublicValues<E1, BS1<E1>, S1<E1>, S2<E1>>>(
+        &public_params_str,
+    )
+    .unwrap();
+
+    let result = proof.verify(public_params).unwrap();
+
+    let result_json;
+    if result {
+        result_json = VerifyResult { success: true }
+    } else {
+        result_json = VerifyResult { success: false }
+    }
+
+    (StatusCode::CREATED, Json(result_json))
 }
 
 // the output to our `create_user` handler
 #[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+struct VerifyResult {
+    success: bool,
 }
