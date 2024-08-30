ff9c841b98e2bb03f8a720151913bc449a06628f
AntoineF4C5 <antoinedcht@hotmail.com>
2024-08-30 13:56:13 +0200

    kept working on rust implementation of send logic

--- server/src/bin/contract_interactions.rs
+++ server/src/bin/contract_interactions.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use secp256k1::SecretKey;
 use web3::api::Eth;
 use web3::signing::SecretKeyRef;
@@ -35,9 +36,8 @@ async fn main() {
         .await;
     println!("Balance: {}", balance);
 
-    let value = 1;
-    let amount = parse_units(&value.to_string(), decimals).unwrap();
-    let amount = U256::from_dec_str(&amount.to_string()).unwrap();
+    let value = U256::from(1);
+    let amount = value * U256::from(10).pow(U256::from(decimals));
 
     let account = SecretKey::from_str(&std::env::var("SENDER_PRIVATE_KEY").unwrap()).unwrap();
     let tx = contract
@@ -63,7 +63,7 @@ impl UsdcContract {
         .unwrap();
         UsdcContract(contract)
     }
-    // Query the totalMatches state variable
+
     pub async fn get_decimals(&self) -> u8 {
         let result: u8 = self
             .0
@@ -72,7 +72,7 @@ impl UsdcContract {
             .unwrap();
         result
     }
-    // Query the lifetimeValue state variable
+
     pub async fn balance_of(&self, address: &String) -> U256 {
         let result: U256 = self
             .0
@@ -88,8 +88,7 @@ impl UsdcContract {
         result
     }
 
-    // Create a match
-    pub async fn transfer(&self, account: SecretKey, to: &String, value: U256) {
+    pub async fn transfer(&self, account: SecretKey, to: &String, value: U256) -> Result<()> {
         // Signed call to create the transaction
         let tx = self
             .0
@@ -107,40 +106,18 @@ impl UsdcContract {
             .await
             .unwrap();
 
+        match tx.status {
+            Some(status) => {
+                if status == 0 {
+                    return Err(Error::TransactionFailed);
+                }
+            }
+            None => {
+                return Err(Error::TransactionFailed);
+            }
+        }
         println!("Transfer completed! Transaction ID: {:?}", tx);
-    }
-    // Join a match
-}
-pub struct Match {
-    match_id: u128,
-    player1: Address,
-    player2: Address,
-    player1_bet: u128,
-    player2_bet: u128,
-    winner: u128,
-    match_complete: bool,
-}
-// Handle the display of the match values
-impl fmt::Display for Match {
-    //Display formatter for the given banner entry
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // Bet values are being output in Wei by default. Must conver them later
-        write!(
-            f,
-            "
-Match ID: {}{}
-Player 1: {}: {} Wei 
-Player 2: {}: {} Wei
-Completed: {}
-Winner: Player {}",
-            "",
-            self.match_id,
-            self.player1,
-            self.player1_bet,
-            self.player2,
-            self.player2_bet,
-            self.match_complete,
-            self.winner
-        )
+
+        Ok(())
     }
 }
